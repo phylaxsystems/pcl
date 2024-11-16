fe5dda039e6108adc9f998c7d0be4e0c26547413
Odysseas.eth <odyslam@gmail.com>
2024-11-16 20:33:33 +0700

    feat: por struct

---
+++ crates/config/src/lib.rs
@@ -0,0 +1,25 @@
+use clap::Parser;
+use alloy_primitives::{Address, BlockHash, BlockNumber, Bytes};
+
+#[derive(Debug, Parser)]
+pub struct PoRInputs {
+    /// The Ethereum address of the assertion adopter that will receive the proof
+    #[arg(short = 'a', long, help = "Ethereum address of the assertion adopter")]
+    assertion_adopter_address: Address,
+
+    /// The Ethereum address of the PoR submitter that will submit the proof
+    #[arg(short = 's', long, help = "Ethereum address of the PoR submitter")]
+    por_submitter: Address,
+
+    /// The block hash to generate the proof for
+    #[arg(short = 'h', long, help = "Block hash to generate proof for")]
+    block_hash: BlockHash,
+
+    /// The block number to generate the proof for
+    #[arg(short = 'n', long, help = "Block number to generate proof for")]
+    block_number: BlockNumber,
+
+    /// The assertion bytes containing the proof data
+    #[arg(short = 'b', long, help = "Assertion bytes containing proof data")]
+    assertion: Bytes,
+}
