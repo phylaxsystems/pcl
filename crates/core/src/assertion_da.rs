81dd59e013a936ff706413c082ce8fe90579596d
GregTheDev <gbcurtis98@outlook.com>
2025-02-23 19:04:45 -0700

    feat: refactor DA (#26)

--- crates/core/src/assertion_da.rs
+++ crates/core/src/assertion_da.rs
@@ -1,18 +1,10 @@
-use crate::{
-    config::CliConfig,
-    error::{DaClientError, DaSubmitError},
-};
-use alloy_primitives::{hex, Bytes, B256};
+use crate::{config::CliConfig, error::DaSubmitError};
+use alloy_primitives::{hex, Bytes};
 use clap::Parser;
 use pcl_common::{args::CliArgs, utils::bytecode};
 use pcl_phoundry::build::BuildArgs;
 
-use jsonrpsee::{
-    core::client::ClientT,
-    http_client::{HttpClient, HttpClientBuilder},
-};
-
-use serde::{Deserialize, Serialize};
+use assertion_da_client::DaClient;
 
 #[derive(Parser)]
 #[clap(
@@ -52,57 +44,5 @@ impl DASubmitArgs {
         println!("Submitted assertion with id: {}", result.id);
         println!("Signature: {}", result.signature);
         Ok(())
-    }
-}
-
-// TODO(Greg): Move this to a crate in the assertion-da repo, leverage here and in the
-// assertion-executor
-#[derive(Debug)]
-pub struct DaClient {
-    client: HttpClient,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct DaSubmissionResponse {
-    id: B256,
-    signature: Bytes,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct DaFetchResponse {
-    bytecode: Bytes,
-    signature: Bytes,
-}
-
-impl DaClient {
-    /// Create a new DA client
-    pub fn new(da_url: &str) -> Result<Self, DaClientError> {
-        let client = HttpClientBuilder::default().build(da_url)?;
-
-        Ok(Self { client })
-    }
-
-    /// Fetch the bytecode for the given assertion id from the DA layer
-    pub async fn fetch_assertion_bytecode(
-        &self,
-        assertion_id: B256,
-    ) -> Result<Bytes, DaClientError> {
-        let response = self
-            .client
-            .request::<DaFetchResponse, &[String]>("da_get_assertion", &[assertion_id.to_string()])
-            .await?;
-
-        Ok(response.bytecode)
-    }
-
-    /// Submit the assertion bytecode to the DA layer
-    pub async fn submit_assertion(
-        &self,
-        code: Bytes,
-    ) -> Result<DaSubmissionResponse, DaClientError> {
-        Ok(self
-            .client
-            .request::<DaSubmissionResponse, &[String]>("da_submit_assertion", &[code.to_string()])
-            .await?)
     }
 }
