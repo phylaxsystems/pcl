13592ee926d00b8b9f38068b2231a2727c358452
GregTheDev <gbcurtis98@outlook.com>
2025-05-02 10:44:25 -0600

    fix: debug log

--- crates/core/src/assertion_submission.rs
+++ crates/core/src/assertion_submission.rs
@@ -79,7 +79,7 @@ impl DappSubmitArgs {
         config: &mut CliConfig,
     ) -> Result<(), DappSubmitError> {
         let projects = self.get_projects(config).await?;
-        println!("Found {}", projects);
+        println!("Found {:#?}", projects);
 
         let assertion_keys_for_submission = config
             .assertions_for_submission
