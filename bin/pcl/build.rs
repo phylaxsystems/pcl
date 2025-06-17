178f212284f51b8ef864b9549d42647df2120bc3
GregTheDev <gbcurtis98@outlook.com>
2025-06-17 15:51:34 -0500

    chore: removed unused env

--- bin/pcl/build.rs
+++ bin/pcl/build.rs
@@ -9,13 +9,6 @@ use vergen_gix::{
 };
 
 pub fn main() -> Result<()> {
-    // Capture DA URL from environment or use default
-    let da_url = std::env::var("PCL_DA_URL")
-        .unwrap_or_else(|_| "https://demo-21-assertion-da.phylax.systems".to_string());
-
-    // Set the DA URL as a build-time environment variable
-    println!("cargo:rustc-env=PCL_BUILD_DA_URL={}", da_url);
-
     Emitter::default()
         .add_instructions(&BuildBuilder::all_build()?)?
         .add_instructions(&CargoBuilder::all_cargo()?)?
